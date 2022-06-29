cc9e4d9ac60f8e83d2cb46dfe240124523ea0719
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-29 16:44:00 -0700

    add imports and placeholders for planning static V of app

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import import firebase from 'firebase/compat/app';
+
+function Login(){
+
+  // placeholder for functions handling login, signup, and logout
+
+  return(
+    <React.Fragment>
+      <h2>placeholder for onsubmit forms</h2>
+      <p>signup</p>
+      <p>login</p>
+      <p>logout</p>
+    </React.Fragment>
+  );
+}
+
+export default Login;
