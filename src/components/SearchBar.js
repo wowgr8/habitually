3e0f707ca4ed83d120fd11a9cf083a8188f65e7e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-29 16:48:27 -0700

    add imports and placeholders for planning static V of app

---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import for search functionality
+
+function SearchBar(){
+  // placeholder for function handling search
+  
+  return(
+    <h2>Searchbar placeholder</h2>
+  );
+}
+
+export default SearchBar;
