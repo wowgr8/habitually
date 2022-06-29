c4abc1aabef0d75f59810fc05367aec157210465
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-29 16:37:50 -0700

    add imports and placeholders for planning static V of app

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import SearchBar from "./SearchBar"; // if we choose to implement
+// import { Link } from "react-router-dom"; // for loggin in
+// import for any image/logo for app
+
+function Header(){
+  return(
+    <React.Fragment>
+      <h1>Header Placeholder: HABITUALLY</h1>
+      <p>SearchBar.js tag placeholder</p>
+      <p>Link tag for Login placeholder</p>
+    </React.Fragment>
+  );
+}
+
+export default Header;
