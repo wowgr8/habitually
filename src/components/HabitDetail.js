38d75b7c68f77ff5adc6344d87c5073822e34ad8
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-29 16:00:36 -0700

    add imports and placeholders for planning static V of app

---
+++ src/components/HabitDetail.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Habit from "./Habit"; //Most likely not be necessary due to props
+
+function HabitDetail(){
+  // placeholder for deconstructed props
+
+  return(
+    <React.Fragment>
+      <h1>Habit Detail</h1>
+      <h3>placeholder for details: HabitName, HabitID, HabitSummary, HabitTimeOpen, HabitTimeGoal, etc.</h3>
+    </React.Fragment>
+  )
+
+}
+
+// PropTypes placeholder
+
+export default HabitDetail;
