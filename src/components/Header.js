389a20539093d5a2b1e2dcbadbc93dbfa9b2ff09
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-29 16:49:28 -0700

    add uncommented SearchBar import and returned it

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import React from "react";
-// import SearchBar from "./SearchBar"; // if we choose to implement
+import SearchBar from "./SearchBar"; // if we choose to implement
 // import { Link } from "react-router-dom"; // for loggin in
 // import for any image/logo for app
 
 function Header(){
   return(
     <React.Fragment>
-      <h1>Header Placeholder: HABITUALLY</h1>
-      <p>SearchBar.js tag placeholder</p>
-      <p>Link tag for Login.js placeholder</p>
+      <h1>Header app name/logo Placeholder: HABITUALLY</h1>
+      <SearchBar />
+      <p>placeholder for Link tag for Login.js</p>
     </React.Fragment>
   );
 }
