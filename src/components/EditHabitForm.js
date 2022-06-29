4948d0ec8d7ddadbb1744739388bd4fc3ac195e8
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-29 15:53:17 -0700

    add imports and placeholders for planning static V of app

---
+++ src/components/EditHabitForm.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReusableForm from "./ReusableForm"; // If we choose to use
+import PropTypes from "prop-types";
+// import { useFirestore } from 'react-redux-firebase';
+
+function EditHabitForm (){
+  // placeholder for firestore reference
+
+  // placeholder for any methods/handling submission of form
+
+  return(
+    <React.Fragment>
+      <h3>Edit Habit Form placeholder</h3>
+    </React.Fragment>
+  )
+}
+
+// placeholder for any propTypes
+
+export default EditHabitForm;
