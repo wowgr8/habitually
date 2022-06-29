c951ec9e696d9c740d3ac3baaee56b8b1dc76766
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-29 16:18:30 -0700

    add imports and placeholders for planning static V of app

---
+++ src/components/Habit.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Habit(){
+  return(
+    <React.Fragment>
+      <h2>Habit placeholder</h2>
+      <p>Place holder for habit details/properties display and onclick method prop</p>
+    </React.Fragment>
+  );
+}
+
+// placeholder for proptypes: id, habitname, HabitSummary, HabitTimeOpen, HabitTimeGoal, etc.
+
+export default Habit;
