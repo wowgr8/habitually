052e8c030a89c65293ad17c01cd9e2f8e60aa468
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-29 17:02:10 -0700

    add imports and placeholders for planning static V of app

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(){
+  return(
+    <React.Fragment>
+      <h2>Reusable form Placeholder. </h2>
+      <p>Needs onSubmit e.handler, form tag w/ input tags for Habit values</p>
+    </React.Fragment>
+  );
+}
+
+// placeholder for proptypes
+
+export default ReusableForm;
