fd153accf86b75ed45b6b3f6dc1ea02ff1120f5a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-29 17:08:42 -0700

    add working render of all components. StaticV of app complete.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,11 @@
 // Will be highest component holding state
 import React from "react";
-import Header from "./Header"; // Parent to searchbar.js
-import Sidebar from "./Sidebar"; // Parent to Habit.js
+import Header from "./Header"; // Parent to searchbar.js & Login.js
+import SideBar from "./SideBar"; // Parent to Habit.js
 import HabitList from "./HabitList"; // Parent to Habit.js
 import EditHabitForm from "./EditHabitForm"; // Parent to ReusableForm.js if we choose to use one.
 import NewHabitForm from "./NewHabitForm"; // Parent to ReusableForm.js if we choose to use one.
 import HabitDetail from "./HabitDetail";
-
-
-
-
 
 class Home extends React.Component {
   //constructor placeholder
@@ -20,13 +16,16 @@ class Home extends React.Component {
     // conditional between components placeholder
 
 
-
-
-
-
     return(
       <React.Fragment>
-        <p>This is the Home component rendering</p>
+        <h1>This is the Home component rendering</h1>
+        <p>Below are placeholders for each component</p>
+        <Header />
+        <SideBar />
+        <EditHabitForm />
+        <HabitDetail />
+        <HabitList />
+        <NewHabitForm />
       </React.Fragment>
     );
   }
