4d42e59f1878a81431ef25d90d50bce1ade53250
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-29 17:07:10 -0700

    add imports and placeholders for planning static V of app

---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import for habitlist if we choose to use
+
+function SideBar(){
+  // placeholder for function handling search
+  
+  return(
+    <React.Fragment>
+      <h2>Sidebar placeholder</h2>
+      <p>Will show list of Habits (perhaps via habitlist.js) at all times. Clicking on one will move you to the that habit via id.</p>
+    </React.Fragment>
+  );
+}
+
+export default SideBar;
