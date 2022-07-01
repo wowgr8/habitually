8b55fd7830206efe783a8fef5f370e264c30a17b
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-30 18:14:17 -0700

    add planning comments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
+import './index.css'; // soon to be replaced with styled objects in App.css
+import App from './components/App'; // soon to be moved out one directory - remove components from this path
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
