97430ccbcf2117cb6e25c0f5c685481219aff9ad
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-13 23:38:47 -0700

    add imports for database and collection/getDocs. Created hooks for users collection and userCollectionRef.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,53 @@
 // Will be highest component holding state
-import React from "react";
+import { React, useEffect, useState } from "react";
 import Header from "./Header"; // Parent to searchbar.js & Login.js
-import SideBar from "./SideBar"; // Parent to Habit.js
+import Sidebar from "./Sidebar"; // Parent to Habit.js
 import HabitList from "./HabitList"; // Parent to Habit.js
 import EditHabitForm from "./EditHabitForm"; // Parent to ReusableForm.js if we choose to use one.
 import NewHabitForm from "./NewHabitForm"; // Parent to ReusableForm.js if we choose to use one.
 import HabitDetail from "./HabitDetail";
+import { db } from '../firebase'
+import { collection, getDocs } from "firebase/firestore";
 
 function Home() {
   //usestate placeholder
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users")
+
+  useEffect(() => {
+    const getUsers = async() => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    }
+    getUsers()
+  }, []);
 
   // methods placeholder
 
   return(
-    <React.Fragment>
+    <div>
+      <div className="usersLoop">
+        {users.map((user) => {
+          return(
+            <div>
+              {" "}
+              <h1>Name: {user.name}</h1>
+              <h1>Id: {user.id}</h1>
+              <h1>Habit: {user.habitName}</h1>
+              <h1>Summary: {user.habitSummary}</h1>
+            </div>
+          )
+        })}
+      </div>
       <h1>This is the Home component rendering</h1>
       <p>Below are placeholders for each component</p>
       <Header />
-      <SideBar />
+      <Sidebar />
       <EditHabitForm />
       <HabitDetail />
       <HabitList />
       <NewHabitForm />
-    </React.Fragment>
+    </div>
   );
 }
 
