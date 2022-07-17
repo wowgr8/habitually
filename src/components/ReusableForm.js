75b4e3d3f080ef9350ad0260cfd94e1798041a6d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-17 15:13:26 -0700

    add useState to react import, add useState hooks for name summary and timeframe. Add onchange e.handler to add habits to db - WIP (will probably move this into NewHabitForm)

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,12 +1,28 @@
-import React from "react";
+import { React, useState } from "react";
 import PropTypes from "prop-types";
 
 function ReusableForm(){
+
+  const [newHabit, setNewHabit] = useState("")
+  const [newSummary, setNewSummary] = useState("")
+  const [newTimeFrame, setNewTimeFrame] = useState("")
+
+
+  const createUser = async () => {
+    
+  }
+
+
   return(
-    <React.Fragment>
+    <div>
       <h2>Reusable form Placeholder. </h2>
       <p>Needs onSubmit e.handler, form tag w/ input tags for Habit values</p>
-    </React.Fragment>
+      {/* <input placeholder="Name..." onChange={(e) => {setNewHabit(e.target.value);}} /> */}
+      <input placeholder="Habit Name..." onChange={(e) => {setNewHabit(e.target.value);}}/>
+      <input placeholder="Habit Summary..." onChange={(e) => {setNewSummary(e.target.value);}}/>
+      <input placeholder="How long to accomplish this goal?..." onChange={(e) => {setNewTimeFrame(e.target.value);}}/>
+      <button onClick={createUser}>Create User</button>
+    </div>
   );
 }
 
