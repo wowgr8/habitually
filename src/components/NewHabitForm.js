c18ee88fd349b55b60ac4535dd72b80bb478022a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-17 23:27:46 -0700

    add createUser() and buttonText to pass as props to reusableform - WIP

--- src/components/NewHabitForm.js
+++ src/components/NewHabitForm.js
@@ -1,20 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ReusableForm from "./ReusableForm";
+import { collection } from "firebase/firestore";
 // import { useFirestore } from 'react-redux-firebase';
 
-function NewHabitForm(){
-  // placeholder to initialize useFireStore()
+function NewHabitForm(props){
 
-  // placeholder for adding new habit values to firestore
+    // const usersCollectionRef = collection(db, "user");
+    //passed in onhandlecreate for button
 
   return(
     <React.Fragment>
-      <h2>NewHabitForm Placeholder. </h2>
-      <p>Will pass handler function to reusable form</p>
-      <ReusableForm />
+      <h2>ADD NEW HABIT </h2>
+      
+      <ReusableForm 
+        createUser = {props.createUser}
+        />
+      <p>Buttontext onSubmit placeholder</p>
     </React.Fragment>
   );
 }
+
+NewHabitForm.propTypes = {
+  createUser: PropTypes.func
+};
 
 export default NewHabitForm;
