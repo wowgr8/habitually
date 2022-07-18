7e333c8aec52d15ff72f65deb378b389b35e0f81
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-18 15:09:08 -0700

    removed unnecessary notes. At this point, new habits can be created successfully.

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,38 +1,20 @@
 import { React } from "react";
 import PropTypes from "prop-types";
-// import { collection, getDocs, addDoc } from "firebase/firestore";
-// import { db } from '../firebase'
 
 function ReusableForm(props){
-  // setNew's are being passed in through props from NEWHABITFORM>JS
-  // const [newHabit, setNewHabit] = useState(() =>"")
-  // const [newSummary, setNewSummary] = useState(() =>"")
-  // const [newTimeFrame, setNewTimeFrame] = useState(() =>"")
 
-  // const usersCollectionRef = collection(db, "user");
-
-  // move this into home.js and pass as prop into this component.
-  // const createUser = async () => {
-  //   await addDoc(usersCollectionRef, { habitName: newHabit, habitSummary: newSummary, habitTimeFrame: newTimeFrame });
-  // }
-  // Deconstructed useState's from NewHabitForm.js
+  // Deconstructed useState setNew's; being passed down from NewHabitForm.js
   const { setNewHabit, setNewSummary, setNewTimeFrame } = props;
-
 
   return(
     <div>
       <h2>Reusable Form:</h2>
       <p>Needs onSubmit e.handler, form tag w/ input tags for Habit values</p>
-      {/* <input placeholder="What habit are you trying to break or build?" onChange={(e) => {setNewHabit(e.target.value);}}/>
-      <input placeholder="Brief summary of why you're committing to this goal:" onChange={(e) => {setNewSummary(e.target.value);}}/>
-      <input placeholder="What is your goal date?" onChange={(e) => {setNewTimeFrame(e.target.value);}}/>
-      <button onClick={props.createUser}>Create User</button> */}
       <form onSubmit={(props.createUser)}>
         <input 
           type='text'
           name="habitName"
           placeholder="What habit are you trying to break or build?" onChange={(e) => {setNewHabit(e.target.value);}}/>
-          {/* placeholder="What habit are you trying to break or build?" onChange={(e) => {setNewHabit(e.target.value);}} */}
         <input 
           type='text'
           name="habitSummary"
@@ -47,7 +29,6 @@ function ReusableForm(props){
   );
 }
 
-// placeholder for proptypes
 ReusableForm.propTypes = {
   createUser: PropTypes.func,
   buttonText: PropTypes.string
