8ff866fb0fcc66cab2ac404607fc0c0d635c0bd0
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-18 16:48:36 -0700

    Add Edit function and button from Home.js

--- src/components/EditHabitForm.js
+++ src/components/EditHabitForm.js
@@ -5,12 +5,21 @@ import PropTypes from "prop-types";
 
 function EditHabitForm (){
   // placeholder for firestore reference
-
+  
   // placeholder for any methods/handling submission of form
+  // function to update habit --- WIP still
+  // const updateHabit = async(id, habitName) => {
+  //   const userDoc = doc(db, "user", id);
+  //   const newFields = {habitName: "Test"};
+  //   await updateDoc(userDoc, newFields);
+  // }
 
+  //WIP - ignore for now.
+  //<button onClick={() => {updateHabit(user.habitName, user.habitSummary, user.habitTimeFrame)}}> Edit Habit </button>
+  
   return(
     <React.Fragment>
-      <h2>EditHabitForm placeholder</h2>
+      <h2>EditHabitForm/REUSABLEFORM placeholder</h2>
     </React.Fragment>
   )
 }
