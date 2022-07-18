31d35f4cd6f535a5668d541d7462c1103942b4c5
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-18 16:48:11 -0700

    removed Edit function and button into EditHabitForm.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,24 +6,19 @@ import HabitList from "./HabitList"; // Parent to Habit.js
 import EditHabitForm from "./EditHabitForm"; // Parent to ReusableForm.js if we choose to use one.
 import NewHabitForm from "./NewHabitForm"; // Parent to ReusableForm.js if we choose to use one.
 import HabitDetail from "./HabitDetail";
-import { db } from '../firebase'
+import { db } from '../firebase';
 import { collection, getDocs, updateDoc, addDoc } from "firebase/firestore";
 
 function Home() {
   //usestate placeholder
   const [users, setUsers] = useState([]);
-  const [newHabit, setNewHabit] = useState(() =>"")
-  const [newSummary, setNewSummary] = useState(() =>"")
-  const [newTimeFrame, setNewTimeFrame] = useState(() =>"")
+  // const [newHabit, setNewHabit] = useState(() =>"")
+  // const [newSummary, setNewSummary] = useState(() =>"")
+  // const [newTimeFrame, setNewTimeFrame] = useState(() =>"")
   // Either restructure collection in database so they are not nested or create a way to access nested collections. 
   const usersCollectionRef = collection(db, "user");
 
-  // function to update habit --- WIP still
-  // const updateHabit = async(id, habitName) => {
-  //   const userDoc = doc(db, "user", id);
-  //   const newFields = {habitName: "Test"};
-  //   await updateDoc(userDoc, newFields);
-  // }
+
 
   useEffect(() => {
     const getUsers = async() => {
@@ -55,13 +50,13 @@ function Home() {
                 <h1>Summary: {user.habitSummary}</h1>
                 <h1>Goal date: {user.habitTimeFrame}</h1>
               </div>
-              {/* I'll come back to finish handling edit functionality after fully implementing creating habits (lifting state and creating props and handlers.). WIP - ignore for now.
-              <button onClick={() => {updateHabit(user.habitName, user.habitSummary, user.habitTimeFrame)}}> Edit Habit </button> */}
+
               <hr></hr>
             </div>
           )
         })}
       </div>
+
       <h1>This is the Home component rendering</h1>
       <p>Below are placeholders for each component</p>
       <Header />
