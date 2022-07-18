2bf02d0505f9a85098ac68234d0d035c98960aae
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-18 15:12:11 -0700

    add db, collection, addDoc imports and setState. Moved in createUser() from home.js. Also commented out unused props.

--- src/components/NewHabitForm.js
+++ src/components/NewHabitForm.js
@@ -1,28 +1,40 @@
-import React from "react";
+import { React, useState } from "react";
 import PropTypes from "prop-types";
 import ReusableForm from "./ReusableForm";
-import { collection } from "firebase/firestore";
-// import { useFirestore } from 'react-redux-firebase';
+import { collection, addDoc } from "firebase/firestore";
+import { db } from '../firebase'
 
-function NewHabitForm(props){
+function NewHabitForm(){
+  const [newHabit, setNewHabit] = useState(() => "")
+  const [newSummary, setNewSummary] = useState(() =>"")
+  const [newTimeFrame, setNewTimeFrame] = useState(() =>"")
 
-    // const usersCollectionRef = collection(db, "user");
-    //passed in onhandlecreate for button
+  const usersCollectionRef = collection(db, "user");
+
+  //passed in onhandlecreate for button
+  const createUser = async (e) => {
+    e.preventDefault();
+    await addDoc(usersCollectionRef, { habitName: newHabit, habitSummary: newSummary, habitTimeFrame: newTimeFrame });
+  }
 
   return(
-    <React.Fragment>
+    <div>
       <h2>ADD NEW HABIT </h2>
       
       <ReusableForm 
-        createUser = {props.createUser}
+        createUser = {createUser}
+        buttonText="CREATE!"
+        setNewHabit = {setNewHabit}
+        setNewSummary = {setNewSummary}
+        setNewTimeFrame = {setNewTimeFrame}
         />
       <p>Buttontext onSubmit placeholder</p>
-    </React.Fragment>
+    </div>
   );
 }
 
-NewHabitForm.propTypes = {
-  createUser: PropTypes.func
-};
+// NewHabitForm.propTypes = {
+//   createUser: PropTypes.func
+// };
 
 export default NewHabitForm;
