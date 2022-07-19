fd67b65e4ff5174b9ec088404dd0842be8eb7f27
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-18 21:28:06 -0700

    add commented out component for dev purposes - WIP

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -1,15 +1,41 @@
-import React from "react";
+import { React, useEffect } from "react";
 import PropTypes from "prop-types";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../firebase'
 
-function Habit(){
-  return(
-    <React.Fragment>
-      <h2>Habit placeholder</h2>
-      <p>Place holder for habit details/properties display and onclick method prop</p>
-    </React.Fragment>
-  );
+function Habit(props){
+
+  // const { users, setUsers, } = props;
+  // const usersCollectionRef = collection(db, "user");
+
+  // useEffect(() => {
+  //   const getUsers = async() => {
+  //     const data = await getDocs(usersCollectionRef);
+  //     setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+  //   }
+  //   getUsers()
+  // }, []);
+
+  // const handleChangingSelectedHabit = (id) => {
+  //   const selectedHabit = usersCollectionRef.filter(users => users.id === id)[0];
+  //   // this.setState({selectedKeg: selectedKeg});
+  // }
+
+  // return(
+  //   <div>
+  //     <h2>Habit placeholder</h2>
+  //     <p>Place holder for habit details/properties display and onclick method prop</p>
+  //     <div id ="cardNew">
+  //       <div onClick = {() => handleChangingSelectedHabit(users.id)} >
+  //         <h1>Habit: {users.habitName}</h1>
+  //         <hr/>
+  //         <h1>Id: {users.id}</h1>
+  //         <h1>Summary: {users.habitSummary}</h1>
+  //         <h1>Goal date: {users.habitTimeFrame}</h1>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
-
-// placeholder for proptypes: id, habitname, HabitSummary, HabitTimeOpen, HabitTimeGoal, etc.
 
 export default Habit;
