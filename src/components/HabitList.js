d423c57b1455c092ec56831e3aa929b0388b46ac
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-18 21:31:06 -0700

    add commented out component for dev purposes - WIP

--- src/components/HabitList.js
+++ src/components/HabitList.js
@@ -1,24 +1,19 @@
 import React from "react";
 import Habit from "./Habit";
 import PropTypes from "prop-types";
-// Here we import hooks functionality from both react-redux and react-redux-firebase.
-//import {useSelector} from 'react-redux';                                                // allows us to extract data from a Redux store.
-//import {useFirestoreConnect, isLoaded, isEmpty} from 'react-redux-firebase';            // allows us to listen for changes to Firestore without using an HOC in a class component.
-                                                                                        //isLoaded() and isEmpty() from react-redux-firebase allow us to check if a collection has been retrieved from Firestore.
 
-function HabitList(){
-  // placeholder for useFirestoreConnect (to specify which collection to listen for changes in Firestore)
+function HabitList(props){
   
-  // placeholder for useSelector (to make state available from store)
+  // These are drilled from home to here to Habit.js --- maybe eliminate prop drill with useContext or defining use state in Habit.js directly.
+  // const { users, setUsers, } = props;
 
-
-  return(
-    <React.Fragment>
-      <h2>Placeholder for HabitList: will hold loop for all Habit.js's</h2>
-      <p>This loop will return props to Habit.js components</p>
-      <Habit />
-    </React.Fragment>
-  )
+  // return(
+  //   <React.Fragment>
+  //     <h2>Placeholder for HabitList: will hold loop for all Habit.js's</h2>
+  //     <p>This loop will return props to Habit.js components</p>
+  //     <Habit users={users} setUsers={setUsers} />
+  //   </React.Fragment>
+  // )
 }
 
 // placeholder for proptypes if we choose to hold any slice of state in redux
