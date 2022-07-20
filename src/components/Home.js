f458d865e8c5d158c64d15ac3ad1e0222e8a860f
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-19 22:39:48 -0700

    add habitList tag to conditional render, removed edithabitform for dev purposes. Also removed commented out code which has been moved to proper comonents - WIP

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,44 +14,14 @@ function Home() {
   const [users, setUsers] = useState([]);
   const [isOpen, setIsOpen] = useState(() => false); // Will handle toggling visible component - atm being passed into components as props.
 
-  // Temp moved this and useEffect into editHabitForm.js for dev purposes
-  // const usersCollectionRef = collection(db, "user");
-
-  // useEffect(() => {
-  //   const getUsers = async() => {
-  //     const data = await getDocs(usersCollectionRef);
-  //     setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-  //   }
-  //   getUsers()
-  // }, []);
-
   return(
     <div>
       {isOpen 
-        ? <EditHabitForm setIsOpen={setIsOpen}
+        ? <HabitList setIsOpen={setIsOpen}
                           isOpen={isOpen}/>
-        // ? <div className="usersLoop">
-        //     {users.map((user) => {
-        //       return(
-        //         <div className="habitCard">
-        //           <div className="container">
-        //             <h1>Id: {user.id}</h1>
-        //             <h1>Habit: {user.habitName}</h1>
-        //             <h1>Summary: {user.habitSummary}</h1>
-        //             <h1>Goal date: {user.habitTimeFrame}</h1>
-        //           </div>
-        //           {/* {"MAYBE ADD ONCLICK BUTTON HERE FOR EDIT/UPDATEHABIT() from edithabitform.js "} */}
-        //           <hr></hr>
-        //         </div>
-        //       )
-        //     })}
-        //   </div>
-
         : <NewHabitForm  setIsOpen={setIsOpen}
                           isOpen={isOpen}/>
       }
-      {/* This div need to be moved to HabitList.js */}
-
 
       {/* <h1>This is the Home component rendering</h1>
       <p>Below are placeholders for each component</p>
