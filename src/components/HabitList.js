f92624c21ef2396997137d5104c226f40b761a86
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-19 22:03:12 -0700

    add notes

--- src/components/HabitList.js
+++ src/components/HabitList.js
@@ -7,13 +7,18 @@ function HabitList(props){
   // These are drilled from home to here to Habit.js --- maybe eliminate prop drill with useContext or defining use state in Habit.js directly.
   // const { users, setUsers, } = props;
 
-  // return(
+  //return(
+
   //   <React.Fragment>
   //     <h2>Placeholder for HabitList: will hold loop for all Habit.js's</h2>
+  //     <h2>loop:</h2>
+  //      <h2>user(id)
+  //          <habit /> 
+  //      </h2>
   //     <p>This loop will return props to Habit.js components</p>
-  //     <Habit users={users} setUsers={setUsers} />
+  //     <Habit users={users} setUsers={setUsers} /> ---- Ignore this line. useEffect will be moved here though. 
   //   </React.Fragment>
-  // )
+  //)
 }
 
 // placeholder for proptypes if we choose to hold any slice of state in redux
