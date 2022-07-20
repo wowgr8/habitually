19a4e53b9208edce9e2f9002682efc219fdfad61
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-19 17:53:00 -0700

    add note for future idea, new isOpen useState which is used in conditional rendering between new & edit habit forms

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,15 +10,20 @@ import { db } from '../firebase';
 import { collection, getDocs, updateDoc, addDoc } from "firebase/firestore";
 
 function Home() {
+
+  ////// Perhaps add a toggle useState and a function with intitial render passed into it..
+  // function initialView(){
+  //   return isOpen, users;
+  // }
+  // const [view, setView] = useState(()=> initialView());
+
+  
   //This users/setUsers and userCollectionRef will need to be moved to HabitList.js
   const [users, setUsers] = useState([]);
-  // const [newHabit, setNewHabit] = useState(() =>"")
-  // const [newSummary, setNewSummary] = useState(() =>"")
-  // const [newTimeFrame, setNewTimeFrame] = useState(() =>"")
-  // Either restructure collection in database so they are not nested or create a way to access nested collections. 
+  // For conditional rendering
+  const [isOpen, setIsOpen] = useState(() => false);
+
   const usersCollectionRef = collection(db, "user");
-
-
 
   useEffect(() => {
     const getUsers = async() => {
@@ -28,43 +33,44 @@ function Home() {
     getUsers()
   }, []);
 
-  // Was in REUSABLEFORM.js
-  // const createUser = async (e) => {
-    // setNewHabit(e.target.value);
-    // setNewSummary(e.target.value);
-    // setNewTimeFrame(e.target.value);
-    // await addDoc(usersCollectionRef, { habitName: newHabit, habitSummary: newSummary, habitTimeFrame: newTimeFrame });
-  // }
-  // methods placeholder
+
 
   return(
     <div>
-      {/* This div need to be moved to HabitList.js */}
-      <div className="usersLoop">
-        {users.map((user) => {
-          return(
-            <div className="habitCard">
-              <div className="container">
-                <h1>Id: {user.id}</h1>
-                <h1>Habit: {user.habitName}</h1>
-                <h1>Summary: {user.habitSummary}</h1>
-                <h1>Goal date: {user.habitTimeFrame}</h1>
-              </div>
-              {/* {"MAYBE ADD ONCLICK BUTTON HERE FOR EDIT/UPDATEHABIT() from edithabitform.js "} */}
-              <hr></hr>
-            </div>
-          )
-        })}
-      </div>
+      {isOpen 
+        ? <EditHabitForm setIsOpen={setIsOpen}
+                          isOpen={isOpen}/>
+        // ? <div className="usersLoop">
+        //     {users.map((user) => {
+        //       return(
+        //         <div className="habitCard">
+        //           <div className="container">
+        //             <h1>Id: {user.id}</h1>
+        //             <h1>Habit: {user.habitName}</h1>
+        //             <h1>Summary: {user.habitSummary}</h1>
+        //             <h1>Goal date: {user.habitTimeFrame}</h1>
+        //           </div>
+        //           {/* {"MAYBE ADD ONCLICK BUTTON HERE FOR EDIT/UPDATEHABIT() from edithabitform.js "} */}
+        //           <hr></hr>
+        //         </div>
+        //       )
+        //     })}
+        //   </div>
 
-      <h1>This is the Home component rendering</h1>
+        : <NewHabitForm  setIsOpen={setIsOpen}
+                          isOpen={isOpen}/> 
+      }
+      {/* This div need to be moved to HabitList.js */}
+
+
+      {/* <h1>This is the Home component rendering</h1>
       <p>Below are placeholders for each component</p>
       <Header />
       <Sidebar />
       <EditHabitForm />
       <HabitDetail />
-      <HabitList />
-      <NewHabitForm  />
+      <HabitList /> */}
+      
     </div>
   );
 }
