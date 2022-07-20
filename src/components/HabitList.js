104036ab4284e9a78bb4e41cfa551e7e89b2926c
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-19 22:33:52 -0700

    add imports, useEffect, db, and defined collectionRef. added loop of habit list.

--- src/components/HabitList.js
+++ src/components/HabitList.js
@@ -1,13 +1,24 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import Habit from "./Habit";
+import { collection, updateDoc, doc, getDocs, setDoc } from "firebase/firestore";
+import { db } from '../firebase';
 import PropTypes from "prop-types";
 
 function HabitList(props){
   
-  // These are drilled from home to here to Habit.js --- maybe eliminate prop drill with useContext or defining use state in Habit.js directly.
-  // const { users, setUsers, } = props;
+  const { isOpen, setIsOpen } = props; // Will this be needed here? WIP
 
-  //return(
+  const usersCollectionRef = collection(db, "user");
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const getUsers = async() => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    }
+    getUsers()
+  }, []);
+
+  return(
 
   //   <React.Fragment>
   //     <h2>Placeholder for HabitList: will hold loop for all Habit.js's</h2>
@@ -18,7 +29,23 @@ function HabitList(props){
   //     <p>This loop will return props to Habit.js components</p>
   //     <Habit users={users} setUsers={setUsers} /> ---- Ignore this line. useEffect will be moved here though. 
   //   </React.Fragment>
-  //)
+    <div className="usersLoop">
+      {users.map((user) => {
+        return(
+          <div className="habitCard">
+            <div className="container">
+              <h1>Id: {user.id}</h1>
+              <h1>Habit: {user.habitName}</h1>
+              <h1>Summary: {user.habitSummary}</h1>
+              <h1>Goal date: {user.habitTimeFrame}</h1>
+            </div>
+            {/* {"MAYBE ADD ONCLICK BUTTON HERE FOR EDIT/UPDATEHABIT() from edithabitform.js "} */}
+            <hr></hr>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 // placeholder for proptypes if we choose to hold any slice of state in redux
