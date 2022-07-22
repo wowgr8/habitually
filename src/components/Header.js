bcf4f5e56df25c24621dbb55c56fc98e8ea11752
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-22 13:35:44 -0700

    add react-router-dom import and linked home and login components.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,23 @@
 import React from "react";
 import SearchBar from "./SearchBar"; // if we choose to implement
-// import { Link } from "react-router-dom"; // for loggin in
+import { Link } from "react-router-dom";
 // import for any image/logo for app
 
 function Header(){
   return(
     <React.Fragment>
       <h1>Header app name/logo Placeholder: HABITUALLY</h1>
-      <SearchBar />
-      <p>placeholder for Link tag for Login.js</p>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Sign In</Link>
+        </li>
+        <div>
+          <SearchBar />
+        </div>
+      </ul>
     </React.Fragment>
   );
 }
