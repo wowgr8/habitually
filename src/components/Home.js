9af12afb1be39171387f8428d8ebebc50c5a4f2e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-22 18:02:40 -0700

    add notes - WIP

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 // Will be highest component holding state
-import { React, useEffect, useState } from "react";
+import { React, useEffect, useState, isLoaded } from "react";
 import Header from "./Header"; // Parent to searchbar.js & Login.js
 import Sidebar from "./Sidebar"; // Parent to Habit.js
 import HabitList from "./HabitList"; // Parent to Habit.js
 import EditHabitForm from "./EditHabitForm"; // Parent to ReusableForm.js if we choose to use one.
 import NewHabitForm from "./NewHabitForm"; // Parent to ReusableForm.js if we choose to use one.
 import HabitDetail from "./HabitDetail";
+import Habit from "./Habit";
 import Login from "./Login";
-import { db } from '../firebase';
+import { db, auth } from '../firebase';
+import { onAuthStateChanged } from "firebase/auth";
+import { Link, Navigate } from "react-router-dom";
 import { collection, getDocs, updateDoc, addDoc } from "firebase/firestore";
 
 function Home() {
@@ -15,25 +18,36 @@ function Home() {
   const [users, setUsers] = useState([]);
   const [isOpen, setIsOpen] = useState(() => false); // Will handle toggling visible component - atm being passed into components as props.
 
+
+
   return(
     <div>
+      {/* <div>
+        <button onClick={monitorAuthState}>check user</button>
+      </div> */}
+
       {isOpen 
-        ? <HabitList setIsOpen={setIsOpen}
+        ? <Habit setIsOpen={setIsOpen}
                           isOpen={isOpen}/>
-        : <Login  setIsOpen={setIsOpen}
+        : <HabitList setIsOpen={setIsOpen}
                           isOpen={isOpen}/>
       }
 
-      {/* <h1>This is the Home component rendering</h1>
-      <p>Below are placeholders for each component</p>
-      <Header />
-      <Sidebar />
-      <EditHabitForm />
-      <HabitDetail />
-      <HabitList /> */}
+      
       
     </div>
   );
 }
 
 export default Home;
+
+
+
+// used to be commented out in the return():
+{/* <h1>This is the Home component rendering</h1>
+<p>Below are placeholders for each component</p>
+<Header />
+<Sidebar />
+<EditHabitForm />
+<HabitDetail />
+<HabitList /> */}
