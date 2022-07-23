4645d7968fd8d02e1c83d6d11ceb480ca602bd97
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-23 15:30:10 -0700

    WIP - Commit before restarting computer and browser

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,23 +3,26 @@ import { onAuthStateChanged } from "firebase/auth";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import { auth } from '../firebase';
 
-function LandingPage() {
+function LandingPage() { //{isAuth, setIsAuth}
 
-  const [isAuth, setIsAuth] = useState(()=> false);
+  const [isAuth, setIsAuth] = useState();
   let navigate = useNavigate();
 
 
-  // When button is clicked. It will take you to login.js
+  // When button is clicked. and user is authorized It will take you to home.js otherwise it wil go to login.js
   const monitorAuthState = async (e) => {
     e.preventDefault();
     onAuthStateChanged(auth, (user) => {
       if(user){
         console.log(user.uid);
         setIsAuth(true);
+        console.log(isAuth + " in landpingPage.js");
         navigate("/");
       }
-      else {
-        console.log("Please sign in");
+      else { 
+        console.log(isAuth);
+        console.log("Please sign in: " + isAuth);
+        console.log(isAuth);
         navigate("/Login");
       }
     })
@@ -30,10 +33,11 @@ function LandingPage() {
   return (
     <div className="LandingPage">
       <div>LandingPage</div>
-      <p>Welcome to Habitually!</p>
-      <h1>A place to track and manage your goals and progression.</h1>
-      <h1>A place to track and manage your goals and progression.</h1>
-      <button className='authButton' onClick={monitorAuthState}>Get Started</button>
+      <h1>Welcome to Habitually!</h1>
+      <h2>A place to track and manage your goals and progression.</h2>
+      <div>
+        <button className='authButton' onClick={monitorAuthState}>Get Started</button>
+      </div>
     </div>
   )
 }
