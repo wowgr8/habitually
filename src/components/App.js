a1d75c802c0da25a556a45ea2cb575e13e27e121
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-22 18:00:40 -0700

    add imports and routes for different components

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,35 @@ import './App.css';
 import Home from './Home';
 import Login from "./Login";
 import Header from './Header';
+import React, { useState } from "react";
+import LandingPage from './LandingPage';
 // import Material UI for styling
 import { 
   BrowserRouter as Router,
   Routes,
   Route 
 } from "react-router-dom";
+import EditHabitForm from './EditHabitForm';
+import NewHabitForm from './NewHabitForm';
+
 
 
 function App() {
+  const [isAuth, setIsAuth] = useState(()=> false);
+
+
+
   return (
     <Router>
       <div className="App">
-        <Header />
-        <div className="card">
-          <div className='row'>
-            <Routes>
-              <Route path="/login" element={<Login />}/>
-              <Route path ="/" element={<Home />}/>
-            </Routes>
-            </div>
-          </div>
+        <Header isAuth={isAuth} setIsAuth={setIsAuth}/>
+        <Routes>
+          <Route path="/LandingPage" element={<LandingPage isAuth={isAuth} setIsAuth={setIsAuth}/>}/>
+          <Route path="/Login" element={<Login isAuth={isAuth} setIsAuth={setIsAuth}/>}/>
+          <Route path ="/NewHabitForm" element={<NewHabitForm isAuth={isAuth} setIsAuth={setIsAuth}/>}/>
+          <Route path ="/EditHabitForm" element={<EditHabitForm isAuth={isAuth} setIsAuth={setIsAuth}/>}/>
+          <Route path ="/" element={<Home isAuth={isAuth} setIsAuth={setIsAuth}/>}/>
+        </Routes>
       </div>
     </Router>
   );
