419a76bd002d67eafa9b46b2744d441804f7112d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-23 15:22:02 -0700

    add conditional render to show login link only, if user is not authenticated.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 import SearchBar from "./SearchBar"; // if we choose to implement
 
 import { Link, Navigate } from "react-router-dom";
 import LandingPage from "./LandingPage";
 // import for any image/logo for app
 
-function Header({setisAuth}){
+function Header({isAuth, setIsAuth}){
 
-
-
+  // const [isAuth, setIsAuth] = useState();
 
   return(
     <div className="container">
       <h1>Header app name/logo Placeholder: HABITUALLY</h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/login">Sign In</Link>
-        </li>
-        <div>
-          <SearchBar />
-        </div>
-      </ul>
+      {!isAuth 
+        ? <>
+          <Link to="/Login">Sign up</Link>
+          </>
+        : (
+          // <>
+          //   <Link to="/">Home</Link>
+          //   <Link to="/Login">Sign In</Link>
+          // </>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/Login">Sign In</Link>
+              </li>
+              <div>
+                <SearchBar />
+              </div>
+            </ul>
+          </div>
+
+
+          )}
+
       {/* Temp here for dev purposes */}
-      <LandingPage/> 
+      {/* <LandingPage/>  */}
     </div>
   );
 }
 
 export default Header;
+
+
+
+
+// conditional render does not seem to be acknowledging when Authentication is true, which should show links for home and sign in
