5cb01e721e4e592914c26323b79653285ba01fb9
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-23 15:23:00 -0700

    add LandingPage conditional render if user is not authenticated. If they are, they can see habitList/Habit

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,31 +10,31 @@ import Habit from "./Habit";
 import Login from "./Login";
 import { db, auth } from '../firebase';
 import { onAuthStateChanged } from "firebase/auth";
-import { Link, Navigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { collection, getDocs, updateDoc, addDoc } from "firebase/firestore";
+import LandingPage from "./LandingPage";
 
-function Home() {
+function Home({isAuth, setIsAuth}) {
   //This users/setUsers and userCollectionRef will need to be moved to HabitList.js
   const [users, setUsers] = useState([]);
   const [isOpen, setIsOpen] = useState(() => false); // Will handle toggling visible component - atm being passed into components as props.
-
+  let navigate = useNavigate();
 
 
   return(
     <div>
-      {/* <div>
-        <button onClick={monitorAuthState}>check user</button>
-      </div> */}
-
-      {isOpen 
-        ? <Habit setIsOpen={setIsOpen}
-                          isOpen={isOpen}/>
-        : <HabitList setIsOpen={setIsOpen}
-                          isOpen={isOpen}/>
+      {!isAuth 
+        ? 
+          <LandingPage />
+        : 
+          (isOpen 
+            ? <Habit setIsOpen={setIsOpen}
+                      isOpen={isOpen}
+                      />
+            : <HabitList setIsOpen={setIsOpen}
+                          isOpen={isOpen}
+                          />)
       }
-
-      
-      
     </div>
   );
 }
