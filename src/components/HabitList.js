1105c847aa861aa63ca13f4eb18a023bc75f3227
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-23 20:32:18 -0700

    removed conditional render. Recreated onHabitSelection(). It now gets selected Habit details successfully.

--- src/components/HabitList.js
+++ src/components/HabitList.js
@@ -8,6 +8,7 @@ function HabitList(props){
   
   const { isOpen, setIsOpen } = props; // Will this be needed here? WIP
   const [isToggle, setIsToggle] = useState(() => false); // dev purposes/ will delete soon
+  const [selectedHabit, setSelectedHabit] = useState(() => null); // dev purposes/ will delete soon
 
   const [users, setUsers] = useState([]);
   const usersCollectionRef = collection(db, "user");
@@ -20,50 +21,55 @@ function HabitList(props){
   }, []);
 
   const onHabitSelection = async (id)=> {
-    // e.preventDefault();
-    // const selectedHabit = usersCollectionRef.filter(doc.id === id)[0];
-
-    setIsToggle(!isToggle);
-    console.log("Toggle to habit.js successful");
     const idRef = doc(db, "user", id);
-    console.log("idRef = " + idRef)
-    const selectedHabit = await getDoc(idRef === id); // issue lies within here.... something here is not tapping into the db correctly
-    // const selectedHabit = () => usersCollectionRef.get(users).filter(doc => doc.id === id)[0]; // WIP on this line. 
-    console.log("selectedHabit success " + selectedHabit);
-    console.log(selectedHabit);
-
+    const docSnap = await getDoc(idRef);
+    if (docSnap.exists()) {
+      setSelectedHabit(docSnap.data());
+      console.log("Document data:", docSnap.data());
+      console.log(selectedHabit)
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+    
+    // idRef.then((habit) => { 
+    //   const selectedHabit = {                     
+    //     habitName: habit.get("habitName"),                
+    //     habitSummary: habit.get("habitSummary"),          
+    //     habitTimeFrame: habit.get("habitTimeFrame"),               
+    //     id: habit.id
+    //   }
+    //   setSelectedHabit(selectedHabit);
+    //   console.log(selectedHabit)
+    // })
   }
+
+
   const goTohabit = (e)=> {
     e.preventDefault();
     setIsToggle(!isToggle);
   }
 
   return(
-    <div className="habitListView">
-      <div className="render">
-        {isToggle 
-          ? <Habit /> 
-          : <div className="habitLoop">
-              <p>habitlist.js</p>
-              <div className="row">
-                {users.map((user) => {
-                  return(
-
-                      <div className="col mb-2">
-                        <Habit
-                          habitClicked={onHabitSelection}
-                          id={user.id}
-                          habitName={user.habitName}
-                          habitSummary={user.habitSummary}
-                          habitTimeFrame={user.habitTimeFrame}
-                          />
-                      <hr></hr>
-                      </div>
-                  )
-                })}
+    <div className="habitLoop">
+      <p>habitlist.js</p>
+      <div className="row">
+        {users.map((user) => {
+          return(
+            <div className="cardHabit">
+              <div className="containerHabit">
+                <Habit
+                  habitClicked={onHabitSelection}
+                  id={user.id}
+                  habitName={user.habitName}
+                  habitSummary={user.habitSummary}
+                  habitTimeFrame={user.habitTimeFrame}
+                  />
+                <hr></hr>
               </div>
             </div>
-        }
+          )
+        })}
       </div>
       <div>
         <button onClick={goTohabit}>Go to habit.js</button>
@@ -75,3 +81,62 @@ function HabitList(props){
 export default HabitList;
 // either let home.js handle toggle to ind. habit.js so that you dont have two <habit> tags here. or
 // 
+
+
+
+
+
+
+// This return works 07/23/22 but why are we rendering habit conditionally two times?
+// we only need to see habit list with a loop and child component with habit key:values passed as props. 
+// return(
+//   <div className="habitListView">
+//     <div className="render">
+//       {isToggle 
+//         ? <Habit /> 
+//         : <div className="habitLoop">
+//             <p>habitlist.js</p>
+//             <div className="row">
+//               {users.map((user) => {
+//                 return(
+
+//                     <div className="col mb-2">
+//                       <Habit
+//                         habitClicked={onHabitSelection}
+//                         id={user.id}
+//                         habitName={user.habitName}
+//                         habitSummary={user.habitSummary}
+//                         habitTimeFrame={user.habitTimeFrame}
+//                         />
+//                     <hr></hr>
+//                     </div>
+//                 )
+//               })}
+//             </div>
+//           </div>
+//       }
+//     </div>
+//     <div>
+//       <button onClick={goTohabit}>Go to habit.js</button>
+//     </div>
+//   </div>
+// )
+
+
+
+
+
+// DOES NOT WORK
+  // const onHabitSelection = async (id)=> {
+  //   // e.preventDefault();
+  //   // const selectedHabit = usersCollectionRef.filter(doc.id === id)[0];
+
+  //   setIsToggle(!isToggle);
+  //   console.log("Toggle to habit.js successful");
+  //   const idRef = doc(db, "user", id);
+  //   console.log("idRef = " + idRef)
+  //   const selectedHabit = await getDoc(idRef === id); // issue lies within here.... something here is not tapping into the db correctly
+  //   // const selectedHabit = () => usersCollectionRef.get(users).filter(doc => doc.id === id)[0]; // WIP on this line. 
+  //   console.log("selectedHabit success " + selectedHabit);
+  //   console.log(selectedHabit);
+  // }
