168df0c22cbe5588d9e00d58d2c9a90450a818ab
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-23 21:50:05 -0700

    add loop showing each habit name

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Habit from "./Habit";
+import { collection, updateDoc, doc, get, getDoc, getDocs, setDoc } from "firebase/firestore";
+import { db } from '../firebase';
 // import for habitlist if we choose to use
 
 function SideBar(){
   // placeholder for function handling search
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "user");
+  useEffect(() => {
+    const getUsers = async() => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    }
+    getUsers()
+  }, []);
   
+  const onHabitSelection = async (id)=> {}
+
   return(
-    <React.Fragment>
+    <div className="sidebar">
       <h2>Sidebar placeholder</h2>
       <p>Will show list of Habits (perhaps via habitlist.js) at all times. Clicking on one will move you to the that habit via id.</p>
-    </React.Fragment>
+      <div className="row">
+        {users.map((user) => {
+          return(
+            <div className="cardHabitSidebar">
+              <div className="containerHabitSidebar">
+                  <p>{user.habitName}</p>
+                <hr></hr>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   );
 }
 
