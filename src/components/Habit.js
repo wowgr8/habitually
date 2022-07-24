677b2f03d3733caf104950a2cae334f5b311e8ce
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-23 20:30:44 -0700

    removed unused imports and comments. Component is ready for styling.

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -1,20 +1,7 @@
-import { React, useEffect } from "react";
+import { React } from "react";
 import PropTypes from "prop-types";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from '../firebase'
 
 function Habit(props){
-
-  const { users, setUsers, } = props;
-  // const usersCollectionRef = collection(db, "user");
-
-  // useEffect(() => {
-  //   const getUsers = async() => {
-  //     const data = await getDocs(usersCollectionRef);
-  //     setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-  //   }
-  //   getUsers()
-  // }, []);
 
   const { habitClicked, id, habitName, habitSummary, habitTimeFrame } = props
 
@@ -22,16 +9,13 @@ function Habit(props){
     <div className="individualHabit">
       <div className="container1">
         <p>Individual Habit.js </p>
-        {/* id is not being read in this component but when it returns back in the HabitList component, it is grabbing the id just fine. -WIP */}
         <div onClick={()=>habitClicked(id)}> 
           <h1>Id: {id}</h1>
-          {console.log("test in habit.js - id: " + id)}
           <h1>Habit: {habitName}</h1>
           <h1>Summary: {habitSummary}</h1>
           <h1>Goal date: {habitTimeFrame}</h1>
         </div>
       </div>
-
     </div>
   );
 }
