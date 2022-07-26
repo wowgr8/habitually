5d2a69c95dd8d87581ca0af5cc5c74c4ca11eae8
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-26 16:11:08 -0700

    add container preventing sidebar from spilling over. Also added notes and removed unused commented code

--- src/components/HabitList.js
+++ src/components/HabitList.js
@@ -7,48 +7,40 @@ import EditHabitForm from "./EditHabitForm";
 
 
 function HabitList(){
-  
-  const [selectedHabit, setSelectedHabit] = useState(); // dev purposes/ will delete soon
-  let navigate = useNavigate(); // To go to edithabit form via onHabitSelection()
 
-  // Render all habits upon rendering page. 
+  let navigate = useNavigate(); 
+  const [selectedHabit, setSelectedHabit] = useState(); // dev purposes/ will delete soon
   const [users, setUsers] = useState([]);
   const usersCollectionRef = collection(db, "user");
+
+
   useEffect(() => {
     const getUsers = async() => {
       const data = await getDocs(usersCollectionRef);
       setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
     }
     getUsers()
-  }, []); // usersCollectionRef should be added? Doesnt seem to hurt. Check again when creating and deleting habits.
+  }, []); 
 
-
-
-
-
-//Uses useCallback to extract the function outside useEffect :source - https://devtrium.com/posts/async-functions-useeffect
+  //Uses useCallback to extract the function outside useEffect :source - https://devtrium.com/posts/async-functions-useeffect
   const onHabitSelection = useCallback( async (id)=> {
     const idRef =  doc(db, "user", id);
     const individualHabit = await getDoc(idRef);
     if (individualHabit.exists()) {   
       console.log("individualHabit");
-      console.log(individualHabit); // null
+      console.log(individualHabit); // null (successful)
       console.log("individualHabit.id")
       console.log(individualHabit.id) // returns id
-      setSelectedHabit(individualHabit.id); // save the values to selectedHabit so they can be passed and used in EdithabitForm.
+      setSelectedHabit(individualHabit.id); // null when useEffect runs the first time per click, true when useEffect runs the second time per click.
       console.log("selectedHabit");
-      // navigate("/EditHabitForm");
     } else {
       console.log("No such document!");
     }
   }, [selectedHabit] )
 
-
-
   useEffect(() => {
     onHabitSelection();
     console.log("useEffect ", selectedHabit);
-    // navigate("/EditHabitForm");
     if (selectedHabit) { // in the beginning, this starts out as `undefined`, which is the same thing as `false`
       // If defined, do this:
       console.log(selectedHabit)
@@ -61,18 +53,14 @@ function HabitList(){
       console.log("selectedHabit is undefined and does not exist. Inside else.")
     }
     console.log("End of useEffect()")
-  },[onHabitSelection]) // dependency could be [selectedHabit] if async function is used in useEffect?
-
-
-
-
-
+  },[onHabitSelection]) 
+  
   return(
-    <div className="habitLoop">
-      <div className="h-full space-y-6 rounded-xl border border-gray-200 bg-white py-8 px-6 2xl:w-[95%]">
+    <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]" id="habitListDiv">
+      <div className="px-6 pt-6 2xl:container">
         {users.map((user) => {
           return(
-            <div>
+            <div className="px-6 pt-6 2xl:container">
                 <Habit
                   habitClicked={onHabitSelection}
                   id={user.id}
@@ -90,28 +78,6 @@ function HabitList(){
 }
 
 export default HabitList;
-
-  // async func within useEffect. and useEffect with state dependency below.
-  // useEffect(()=>{
-  //   const onHabitSelection = async(id ) => {
-  //     const idRef = doc(db, "user", id);
-  //     const individualHabit = await getDoc(idRef);
-  //     if (individualHabit.exists()) { 
-  //       return individualHabit
-  //     }
-  //   } 
-  //   onHabitSelection()
-  // }, [])
-
-  // useEffect(() => {
-  //   onHabitSelection()
-  //   console.log("useEffect");
-
-  // },[selectedHabit]) // also tried onHabitSelection as dependency. Problem here is I havent thought of a way to call onHabitSelection w/ onClick due to scope.
-
-
-
-
 
   // Pre-Tailwind
   // <p>habitlist.js</p>
