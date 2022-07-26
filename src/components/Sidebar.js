e71c757a0cc5bad2efcf1b0e1c9528766eab49c9
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-26 16:15:12 -0700

    added styling to prevent sidebar from spilling into home component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -18,7 +18,8 @@ function SideBar({isAuth, setIsAuth}){
     getUsers()
   }, []);
   
-  // const onHabitSelection = async (id)=> {}  WIP
+  // set up onhabit selection here. 
+  // you can set up a utils dir/file and export authSign out and onHabitSelection to habitlist, here, and login.....
 
   let navigate = useNavigate();
 
@@ -40,7 +41,7 @@ function SideBar({isAuth, setIsAuth}){
 
   return(
     <div className="sidebarrr">
-      <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
+      <aside className= "fixed top-0 z-10 ml-[-100%] flex h-screen w-full flex-col justify-between border-r bg-white px-6 pb-3 transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
         <div>
         <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">HABITUALLY</h1>
           {/* <div className="-mx-6 px-6 py-4">    THIS HOLDS PROFILE IMAGE
@@ -50,7 +51,7 @@ function SideBar({isAuth, setIsAuth}){
           </div> */}
           <div className="mt-8 text-center">
             {/* <img src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp" alt="" className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"> */}
-            <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">Cynthia J. Watts</h5>
+            <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">User #1</h5>
             <span className="hidden text-gray-400 lg:block">Admin</span>
           </div>
           <ul className="space-y-2 tracking-wide mt-8">
@@ -68,56 +69,6 @@ function SideBar({isAuth, setIsAuth}){
                 </li>
               )
             })}
-
-
-
-
-            {/* <li>
-              <a href="#" aria-label="dashboard" className="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-sky-600 to-cyan-400">
-                <svg className="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
-                  <path d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z" className="fill-current text-cyan-400 dark:fill-slate-600"></path>
-                  <path d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z" className="fill-current text-cyan-200 group-hover:text-cyan-300"></path>
-                  <path d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z" className="fill-current group-hover:text-sky-300"></path>
-                </svg>
-                <span className="-mr-1 font-medium">Dashboard</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                  <path className="fill-current text-gray-300 group-hover:text-cyan-300" fill-rule="evenodd" d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z" clip-rule="evenodd" />
-                  <path className="fill-current text-gray-600 group-hover:text-cyan-600" d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
-                </svg>
-                  <span className="group-hover:text-gray-700">Categories</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                  <path className="fill-current text-gray-600 group-hover:text-cyan-600" fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd" />
-                  <path className="fill-current text-gray-300 group-hover:text-cyan-300" d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
-                </svg>
-                <span className="group-hover:text-gray-700">Reports</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                  <path className="fill-current text-gray-600 group-hover:text-cyan-600" d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
-                  <path className="fill-current text-gray-300 group-hover:text-cyan-300" d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
-                </svg>
-                <span className="group-hover:text-gray-700">Other data</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                  <path className="fill-current text-gray-300 group-hover:text-cyan-300" d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
-                  <path className="fill-current text-gray-600 group-hover:text-cyan-600" fill-rule="evenodd" d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z" clip-rule="evenodd" />
-                </svg>
-                <span className="group-hover:text-gray-700">Finance</span>
-              </a>
-            </li> */}
           </ul>
         </div>
         <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
@@ -130,22 +81,27 @@ function SideBar({isAuth, setIsAuth}){
         </div>
       </aside>
 
-      {/* <h2>Sidebar placeholder</h2>
-      <p>Will show list of Habits (perhaps via habitlist.js) at all times. Clicking on one will move you to the that habit via id.</p>
-      <div classNameName="row">
-        {users.map((user) => {
-          return(
-            <div classNameName="cardHabitSidebar">
-              <div classNameName="containerHabitSidebar">
-                  <p>{user.habitName}</p>
-                <hr></hr>
-              </div>
-            </div>
-          )
-        })}
-      </div> */}
+
     </div>
   );
 }
 
 export default SideBar;
+
+
+// Pre-Tailwind:
+
+// <h2>Sidebar placeholder</h2>
+// <p>Will show list of Habits (perhaps via habitlist.js) at all times. Clicking on one will move you to the that habit via id.</p>
+// <div classNameName="row">
+//   {users.map((user) => {
+//     return(
+//       <div classNameName="cardHabitSidebar">
+//         <div classNameName="containerHabitSidebar">
+//             <p>{user.habitName}</p>
+//           <hr></hr>
+//         </div>
+//       </div>
+//     )
+//   })}
+// </div>
