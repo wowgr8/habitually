4d2d64f4eace10b3017bc224f5e8dba7319293fd
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-26 19:03:53 -0700

    add all useState declarations

--- src/utils/Context.js
+++ src/utils/Context.js
@@ -3,10 +3,19 @@ import React, { useState } from "react";
 export const Context = React.createContext();
 
 export const ContextProvider = ({ children }) => {
-  const [selectedHabit, setSelectedHabit] = useState();
+  const [ users, setUsers ] = useState([]);
+  const [ selectedHabit, setSelectedHabit ] = useState();
+  const [ newHabit, setNewHabit ] = useState(() => "")
+  const [ newSummary, setNewSummary ] = useState(() =>"")
+  const [ newTimeFrame, setNewTimeFrame ] = useState(() =>"")
+  const [ isOpen, setIsOpen ] = useState();
+  const [ isAuth, setIsAuth ] = useState();
 
   return (
-    <Context.Provider value={{ selectedHabit, setSelectedHabit }}>
+    <Context.Provider value={{ selectedHabit, setSelectedHabit, users, setUsers, 
+                                newHabit, setNewHabit, newSummary, setNewSummary, 
+                                newTimeFrame, setNewTimeFrame, isOpen, setIsOpen, 
+                                isAuth, setIsAuth }}>
       {children}
     </Context.Provider>
   );
