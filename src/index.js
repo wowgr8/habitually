8e476d61750ae560a89197ee5e84eebaca4f902d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-26 17:37:41 -0700

    add import for ContextProvider and replaced React.Strictmode with it.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css'; // soon to be replaced with styled objects in App.css
 import App from './components/App'; // soon to be moved out one directory - remove components from this path
 import reportWebVitals from './reportWebVitals';
+import { ContextProvider } from './utils/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ContextProvider>
     <App />
-  </React.StrictMode>
+  </ContextProvider>
 );
 
 reportWebVitals();
