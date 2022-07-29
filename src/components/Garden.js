33d012812a9f76c4e30a6fbb7a548d397cb137c7
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-29 16:00:43 -0700

    add garden as centered background

--- src/components/Garden.js
+++ src/components/Garden.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import image from '../img/gardenV1.jpeg'
+
+//const gardenPic = new URL('../img/gardenV1.jpeg', import.meta.url)
 
 function Garden() {
   return (
-    <div>Garden</div>
+  <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%] rounded">
+    <div className="px-6 pt-6 2xl:container rounded">
+      <div className="">
+        <div className="hero min-h-screen " style={{ backgroundImage:`url(${image})`}}>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
   )
 }
 
