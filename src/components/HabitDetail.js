333b315385bb518c441fb8c0decd6ffe73a38c78
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-28 19:27:49 -0700

    add table.

--- src/components/HabitDetail.js
+++ src/components/HabitDetail.js
@@ -50,34 +50,76 @@ function HabitDetail(){
   interval = setInterval(countDown, second);
 
 
-  useEffect(()=> {
+  // useEffect(()=> {
       
 
 
-  }, [day, hour, minute, second]);
+  // }, [day, hour, minute, second]);
     
 
 
   return(
-      <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]" >
-        <div className="px-6 pt-6 2xl:container">
-          <div>
-            <h2>HabitDetail placeholder</h2>
-            <h1>Id: {selectedHabit}</h1>
-            <h1>Habit: {habitBody.habitName}</h1>
-            <h1>Summary: {habitBody.habitSummary}</h1>
-            <h1>Goal date: {new Date(habitBody.habitTimeFrame.seconds * 1000).toLocaleDateString("en-US")}</h1>
-            <h1>Created date: {new Date(habitBody.createdAt.seconds * 1000).toLocaleDateString("en-US")}</h1>
-          </div>
 
-          <div class="date-countdown-container">
-            <h2 id="day"></h2>
-            <p>:</p>
-            <h2 id="hour"></h2>
-            <p>:</p>
-            <h2 id="minute"></h2>
-            <p>:</p>
-            <h2 id="second"></h2>
+      <div className="h-full ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]" >
+        <div className="h-full px-6 pt-6 2xl:container ">
+          <div className="h-full  bg-white shadow-md rounded px-8 pt-16 pb-18 mb-4  flex-col ">
+            <div class="overflow-x-auto relative">
+              <table class="w-full text-lg text-left text-white-500 ">
+                <thead class="text-lg text-gray-700 uppercase bg-white-50  ">
+                  <tr>
+                    <th scope="col" class="py-3 px-6">
+                      DETAILS
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="bg-white border-b">
+                    <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap ">
+                      Id  
+                    </th>
+                    <td class="py-4 px-6">
+                      {selectedHabit}
+                    </td>
+                  </tr>
+                  <tr class="bg-white border-b">
+                    <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap ">
+                      Name 
+                    </th>
+                    <td class="py-4 px-6">
+                      {habitBody.habitName}
+                    </td>
+                  </tr>
+                  <tr class="bg-white border-b">
+                    <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap ">
+                      Summary
+                    </th>
+                    <td class="py-4 px-6">
+                      {habitBody.habitSummary}
+                    </td>
+                  </tr>
+                  <tr class="bg-white border-b">
+                    <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap ">
+                      Created  
+                    </th>
+                    <td class="py-4 px-6">
+                      {new Date(habitBody.habitTimeFrame.seconds * 1000).toLocaleDateString("en-US")}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+
+                    <div className="">
+                      <h2 id="day"></h2>
+                      <p>:</p>
+                      <h2 id="hour"></h2>
+                      <p>:</p>
+                      <h2 id="minute"></h2>
+                      <p>:</p>
+                      <h2 id="second"></h2>
+                    </div>
+
+                    
+            </div>
           </div>
         </div>
       </div>
