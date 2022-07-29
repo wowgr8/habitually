984f3d04c19a0d29688ccaad1598a93420dd2b0f
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-28 17:42:00 -0700

    add getElementbyId methods to render timer in UI

--- src/components/HabitDetail.js
+++ src/components/HabitDetail.js
@@ -13,43 +13,72 @@ function HabitDetail(){
 
 
 
-    let interval;
-    const eventDay = new Date(habitBody.habitTimeFrame.seconds * 1000); // returns obj.
-    // defining second minute hour and day
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+  let interval;
+  const eventDay = new Date(habitBody.habitTimeFrame.seconds * 1000); // returns obj.
+  // defining second minute hour and day
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    const countDown = () => {
-      let now = new Date() /1 ;
-      let timeSpan = eventDay - now;
-      if (timeSpan <= 0) {
-        console.log("Today is the event day");
-        clearInterval(interval);
-        return;
-      } else {
-        const days = Math.floor(timeSpan / day)
-        const hours = Math.floor((timeSpan % day) / hour)
-        const minutes = Math.floor((timeSpan % hour) / minute)
-        const seconds = Math.floor((timeSpan % minute) / second)
+
+  const countDown = () => {
+    let now = new Date() /1 ;
+    let timeSpan = eventDay - now;
+    if (timeSpan <= 0) {
+      console.log("Today is the event day");
+      clearInterval(interval);
+      return;
+    } else {
+      const days = Math.floor(timeSpan / day);
+      const hours = Math.floor((timeSpan % day) / hour);
+      const minutes = Math.floor((timeSpan % hour) / minute);
+      const seconds = Math.floor((timeSpan % minute) / second);
+
+      let dayField = document.getElementById('day');
+      let hourField = document.getElementById('hour');
+      let minuteField = document.getElementById('minute');
+      let secondField = document.getElementById('second');
       
-        console.log("Days: " + days,  " Hours: " +  hours, " Minutes: " +  minutes,  " Seconds: " +  seconds);
-      }
+      //console.log("Days: " + days,  " Hours: " +  hours, " Minutes: " +  minutes,  " Seconds: " +  seconds);
+      dayField.innerHTML = days;
+      hourField.innerHTML = hours;
+      minuteField.innerHTML = minutes;
+      secondField.innerHTML = seconds;
     }
-    interval = setInterval(countDown, second);
+  }
+  interval = setInterval(countDown, second);
+
+
+  useEffect(()=> {
+      
+
+
+  }, [day, hour, minute, second]);
     
 
 
   return(
       <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]" >
         <div className="px-6 pt-6 2xl:container">
-          <h2>HabitDetail placeholder</h2>
-                <h1>Id: {selectedHabit}</h1>
-                <h1>Habit: {habitBody.habitName}</h1>
-                <h1>Summary: {habitBody.habitSummary}</h1>
-                <h1>Goal date: {new Date(habitBody.habitTimeFrame.seconds * 1000).toLocaleDateString("en-US")}</h1>
-                <h1>Created date: {new Date(habitBody.createdAt.seconds * 1000).toLocaleDateString("en-US")}</h1>
+          <div>
+            <h2>HabitDetail placeholder</h2>
+            <h1>Id: {selectedHabit}</h1>
+            <h1>Habit: {habitBody.habitName}</h1>
+            <h1>Summary: {habitBody.habitSummary}</h1>
+            <h1>Goal date: {new Date(habitBody.habitTimeFrame.seconds * 1000).toLocaleDateString("en-US")}</h1>
+            <h1>Created date: {new Date(habitBody.createdAt.seconds * 1000).toLocaleDateString("en-US")}</h1>
+          </div>
+
+          <div class="date-countdown-container">
+            <h2 id="day"></h2>
+            <p>:</p>
+            <h2 id="hour"></h2>
+            <p>:</p>
+            <h2 id="minute"></h2>
+            <p>:</p>
+            <h2 id="second"></h2>
+          </div>
         </div>
       </div>
   )
