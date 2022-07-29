35e8bbf23f0b7a4fa704931fb5d327513b03a50a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-29 13:58:09 -0700

    add useEffect to successfully unmount timer.

--- src/components/HabitDetail.js
+++ src/components/HabitDetail.js
@@ -1,4 +1,4 @@
-import { React, useContext, useState } from "react";
+import { React, useContext, useEffect, useState } from "react";
 import { Context } from "../utils/Context";
 import { useNavigate } from 'react-router-dom';
 
@@ -20,36 +20,44 @@ function HabitDetail(){
   const hour = minute * 60;
   const day = hour * 24;
 
-  const countDown = () => {
-    const eventDay = new Date(habitBody.habitTimeFrame.seconds * 1000); // returns obj.
-    let now = new Date() /1 ;
-    let timeSpan = eventDay - now;
-    if (timeSpan <= 0) {
-      console.log("Today is the event day");
-      clearInterval(interval);
-      return;
-      } else {
-      const days = Math.floor(timeSpan / day);
-      const hours = Math.floor((timeSpan % day) / hour);
-      const minutes = Math.floor((timeSpan % hour) / minute);
-      const seconds = Math.floor((timeSpan % minute) / second);
-
-      let dayField = document.getElementById('day');
-      let hourField = document.getElementById('hour');
-      let minuteField = document.getElementById('minute');
-      let secondField = document.getElementById('second');
-      
-      dayField.innerHTML = days;
-      hourField.innerHTML = hours;
-      minuteField.innerHTML = minutes;      
-      secondField.innerHTML = seconds;
-      setD(dayField.innerHTML = days);
-      setH(hourField.innerHTML = hours);
-      setM(minuteField.innerHTML = minutes);
-      setS(secondField.innerHTML = seconds);
+  useEffect(()=>{
+    const countDown = () => {
+      const eventDay = new Date(habitBody.habitTimeFrame.seconds * 1000); // returns obj.
+      let now = new Date() /1 ;
+      let timeSpan = eventDay - now;
+      if (timeSpan <= 0) {
+        console.log("Today is the event day");
+        clearInterval(interval);
+        return;
+        } else {
+        const days = Math.floor(timeSpan / day);
+        const hours = Math.floor((timeSpan % day) / hour);
+        const minutes = Math.floor((timeSpan % hour) / minute);
+        const seconds = Math.floor((timeSpan % minute) / second);
+  
+        let dayField = document.getElementById('day');
+        let hourField = document.getElementById('hour');
+        let minuteField = document.getElementById('minute');
+        let secondField = document.getElementById('second');
+        
+        dayField.innerHTML = days;
+        hourField.innerHTML = hours;
+        minuteField.innerHTML = minutes;      
+        secondField.innerHTML = seconds;
+        setD(dayField.innerHTML = days);
+        setH(hourField.innerHTML = hours);
+        setM(minuteField.innerHTML = minutes);
+        setS(secondField.innerHTML = seconds);
+      }
     }
-  }
-  interval = setInterval(countDown, second);
+    interval = setInterval(countDown, second);
+
+    return()=>{
+      clearInterval(interval);
+    }
+  }, []);
+
+  // add this to useEffect return() => { clearInterval(interval) } // to unmount.
 
   let navigate = useNavigate();
 
