c410f0ad298aa77692b244e707ee6fdd363cb749
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-29 20:17:45 -0700

    add working url for grabbing a random pokemon image. No API necessary

--- src/components/Garden.js
+++ src/components/Garden.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import image from '../img/gardenV1.jpeg'
+import React, { useState, useEffect } from 'react';
+import image from '../img/gardenV1.jpeg';
+import axios from 'axios';
 
-//const gardenPic = new URL('../img/gardenV1.jpeg', import.meta.url)
 
 function Garden() {
+  
+  const [ pokemonImg, setPokemonImg ] = useState([]);
+  const [ dbPokemon, setDbPokemon ] = useState([])
+
+  const randomNum = Math.floor(Math.random() * 151) +1;
+  const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${randomNum}.png?raw=true`;
+
+  // useEffect(()=> {
+  //   axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`).then(res => {
+  //     res.data.sprites
+  //   })
+  // }, [])
+
+
+
+
   return (
   <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%] rounded">
     <div className="px-6 pt-6 2xl:container rounded">
       <div className="">
         <div className="hero min-h-screen " style={{ backgroundImage:`url(${image})`}}>
-
+          <p>HELLO world</p>
+          <img className=" w-80 h-80" src={imageUrl}/>
         </div>
       </div>
     </div>
   </div>
-
-
-
-
-
   )
 }
 
