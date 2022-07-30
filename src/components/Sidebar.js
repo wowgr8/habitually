94765b2a74500435eed80d479055fc6a27532597
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-30 15:13:16 -0700

    add key value to loop

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -60,14 +60,14 @@ function SideBar(){
           <ul className="space-y-2 tracking-wide mt-8">
             {users.map((user) => {
               return(
-                <li>
+                <li key={user.id}>
                   <a href="#" aria-label="dashboard" className="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-sky-600 to-cyan-400">
                     <svg className="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
                       <path d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z" className="fill-current text-cyan-400 dark:fill-slate-600"></path>
                       <path d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z" className="fill-current text-cyan-200 group-hover:text-cyan-300"></path>
                       <path d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z" className="fill-current group-hover:text-sky-300"></path>
                     </svg>
-                    <span className="-mr-1 font-medium">{user.habitName}</span>
+                    <span  className="-mr-1 font-medium">{user.habitName}</span>
                   </a>
                 </li>
               )
