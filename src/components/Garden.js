4a0316ef9043ce75c77fa23ca23ac4eeda3b866a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-29 20:46:42 -0700

    add api call to render pokemon name

--- src/components/Garden.js
+++ src/components/Garden.js
@@ -5,17 +5,19 @@ import axios from 'axios';
 
 function Garden() {
   
-  const [ pokemonImg, setPokemonImg ] = useState([]);
+  const [ pokemonName, setPokemonName ] = useState([]);
   const [ dbPokemon, setDbPokemon ] = useState([])
 
   const randomNum = Math.floor(Math.random() * 151) +1;
   const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${randomNum}.png?raw=true`;
 
-  // useEffect(()=> {
-  //   axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`).then(res => {
-  //     res.data.sprites
-  //   })
-  // }, [])
+  useEffect(()=> {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`).then(res => {
+      setPokemonName(res.data.forms)
+    })
+    console.log(pokemonName)
+    console.log(pokemonName[0].name) 
+  }, []) 
 
 
 
@@ -24,9 +26,12 @@ function Garden() {
   <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%] rounded">
     <div className="px-6 pt-6 2xl:container rounded">
       <div className="">
-        <div className="hero min-h-screen " style={{ backgroundImage:`url(${image})`}}>
+        <div className="hero min-h-screen" style={{ backgroundImage:`url(${image})`}}>
           <p>HELLO world</p>
-          <img className=" w-80 h-80" src={imageUrl}/>
+          <img data-tooltip-target="tooltip-no-arrow" className=" w-80 h-80" src={imageUrl}/>
+          <div >
+            <h1>{pokemonName[0].name} </h1>
+          </div>
         </div>
       </div>
     </div>
