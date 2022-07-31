f3420341ae9dec33cf15b28c82a9e997613910cd
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-30 20:34:51 -0700

    add header, summary body, and button for card.

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -7,38 +7,23 @@ function Habit(props){
 
   return(
     <section>
-      {/* add nice spacing between list in HabitList view: md:col-span-2 sm:col-span-3 lg:col-span-1 ml-auto lg:py-2.5 2xl:w-[50%] mt-25 */}
       <ul className=" ">
         <li x-for="">
-
-          <a  className="">
-            <div className="">
-              <div onClick={()=>habitClicked(id)} className="">
-                {/* <h1>Id: {id}</h1> */}
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{habitName}</h5>
-                <h1>Summary: {habitSummary}</h1>
-                <h1>Goal date: {habitTimeFrame}</h1>
-                <h1>Created: {createdAt}</h1>
-                <div>
-                  <div className="sr-only text-grey-darker">Habit Name</div>
-                  <div className="">
-                    {habitName}
-                  </div>
-                </div>
-                <div>
-                  <div className="sr-only text-grey-darker">Habit TimeFrame</div>
-                  <div className="test-black">{habitTimeFrame}</div>
-                </div>
+          <a className="">
+            <div onClick={()=>habitClicked(id)} className="">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{habitName}</h5>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{habitSummary}</p>
+              <div class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                See details
+                <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
               </div>
             </div>
           </a>
         </li>
       </ul>
     </section>
-  );
+  )
 }
-
-
 
 Habit.propTypes = {
   id: PropTypes.string,
