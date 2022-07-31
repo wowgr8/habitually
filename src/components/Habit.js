56b3af6f81acbf8867694aaa384816688253869c
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-30 20:26:08 -0700

    add bold habit Name title

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -6,17 +6,16 @@ function Habit(props){
   const { habitClicked, id, habitName, habitSummary, habitTimeFrame, createdAt } = props
 
   return(
-    // the conatiner is being set in habitList.js 
     <section>
       {/* add nice spacing between list in HabitList view: md:col-span-2 sm:col-span-3 lg:col-span-1 ml-auto lg:py-2.5 2xl:w-[50%] mt-25 */}
-      <ul className="">
+      <ul className=" ">
         <li x-for="">
-          {/* adds to the nice spacing when rendered back into HabitList.js: bg-white shadow-md rounded mb-4 flex flex-col */}
+
           <a  className="">
             <div className="">
-              <div onClick={()=>habitClicked(id)} className=" items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-sky-600 to-cyan-400">
-                <h1>Id: {id}</h1>
-                <h1>Habit: {habitName}</h1>
+              <div onClick={()=>habitClicked(id)} className="">
+                {/* <h1>Id: {id}</h1> */}
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{habitName}</h5>
                 <h1>Summary: {habitSummary}</h1>
                 <h1>Goal date: {habitTimeFrame}</h1>
                 <h1>Created: {createdAt}</h1>
@@ -39,17 +38,7 @@ function Habit(props){
   );
 }
 
-{/* <div className="individualHabit">
-  <div className="container1">
-    <p>Individual Habit.js </p>
-    <div onClick={()=>habitClicked(id)}> 
-      <h1>Id: {id}</h1>
-      <h1>Habit: {habitName}</h1>
-      <h1>Summary: {habitSummary}</h1>
-      <h1>Goal date: {habitTimeFrame}</h1>
-    </div>
-  </div>
-</div> */}
+
 
 Habit.propTypes = {
   id: PropTypes.string,
@@ -61,3 +50,36 @@ Habit.propTypes = {
 
 export default Habit;
 
+
+
+// pre-styling:
+
+// <section>
+// add nice spacing between list in HabitList view: md:col-span-2 sm:col-span-3 lg:col-span-1 ml-auto lg:py-2.5 2xl:w-[50%] mt-25
+// <ul className=" ">
+//   <li x-for="">
+
+//     <a  className="">
+//       <div className="">
+//         <div onClick={()=>habitClicked(id)} className="">
+//           <h1>Id: {id}</h1>
+//           <h1>Habit: {habitName}</h1>
+//           <h1>Summary: {habitSummary}</h1>
+//           <h1>Goal date: {habitTimeFrame}</h1>
+//           <h1>Created: {createdAt}</h1>
+//           <div>
+//             <div className="sr-only text-grey-darker">Habit Name</div>
+//             <div className="">
+//               {habitName}
+//             </div>
+//           </div>
+//           <div>
+//             <div className="sr-only text-grey-darker">Habit TimeFrame</div>
+//             <div className="test-black">{habitTimeFrame}</div>
+//           </div>
+//         </div>
+//       </div>
+//     </a>
+//   </li>
+// </ul>
+// </section>
