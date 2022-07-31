a5d3a3b24af4a0dc8e7ba0c5e18b818d77d3f580
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-30 23:05:26 -0700

    add dark background, removed redundant styling and divs. Also centered buttons

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -10,30 +10,20 @@ function ReusableForm(props){
 
   return(
   <div className="">
-    <div className="px-6 pt-6 2xl:container">
+    <div className="">
     {/*  add to div below?:px-8 pt-16 pb-18 mb-4 flex flex-col (from login styling)*/}
-      <div class="bg-white shadow-md rounded ">
-        <div class=" ">
-          {/* <!-- Row --> I think this div below need to be cleaned up. w-full and flex may need to be removed*/}
-          <div class="w-full xl:w-3/4 lg:w-11/12 flex">
-            {/* <!-- Col -->  */}
-              {/* <div
-                class="w-full h-auto bg-gray-400 hidden lg:block lg:w-5/12 bg-cover rounded-l-lg"
-                style="background-image: url('https://source.unsplash.com/Mv9hjnEUHR4/600x800')"
-              ></div> */}
-            {/* <!-- Col --> */}
 
-            {/* div below could hold line 13 */}
-            <div class="     ">
-              <h3 class="pt-4 text-2xl text-center">Enter details!</h3>
-              <form onSubmit={(props.onSubmissionHandler)} class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+
+      <div class="bg-white dark:bg-gray-800 shadow-md rounded-lg shadow-md  border border-gray-200 ">
+              <h3 class="pt-4 text-2xl text-center">NEW DETAILS</h3>
+              <form onSubmit={(props.onSubmissionHandler)} class="px-12 pt-6 pb-8 mb-4 bg-white dark:bg-gray-800 rounded">
                 <div class="mb-4 md:flex md:justify-between">
                   <div class="mb-4 md:mr-2 md:mb-0">
-                    <label class="block mb-2 text-sm font-bold text-gray-700" for="firstName">
+                    <label class="block mb-2 text-sm font-bold text-gray-400" for="firstName">
                       Name Your Habit
                     </label>
                     <input
-                      class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                      class="w-full px-3 py-2 text-sm leading-tight text-gray-400 border rounded shadow appearance-none input input-bordered input-md focus:outline-none focus:shadow-outline pr-20 dark:border-blue-500"
                       id="habitName"
                       name="habitName"
                       type="text"
@@ -41,11 +31,11 @@ function ReusableForm(props){
                       onChange={(e) => {setNewHabit(e.target.value);}}/>
                   </div>
                   <div class="md:ml-2">
-                    <label class="block mb-2 text-sm font-bold text-gray-700" for="lastName">
+                    <label class="block mb-2 text-sm font-bold text-gray-400" for="lastName">
                       Time Frame
                     </label>
                     <input
-                      class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                      class="w-full px-3 py-2 text-sm leading-tight text-gray-400 border rounded shadow appearance-none focus:outline-none focus:shadow-outline input input-bordered dark:border-blue-500 input-md pl-20 dark:border-blue-500"
                       id="habitTimeFrame"
                       name="habitTimeFrame"
                       type="datetime-local"
@@ -54,11 +44,11 @@ function ReusableForm(props){
                   </div>     
                 </div>
                 <div class="mb-4">
-                  <label class="block mb-2 text-sm font-bold text-gray-700" for="email">
+                  <label class="block mb-2 text-sm font-bold text-gray-400" for="email">
                     Summary
                   </label>
                   <input
-                    class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                    class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-400 border rounded shadow appearance-none focus:outline-none focus:shadow-outline textarea textarea-info dark:border-blue-500"
                     id="habitSummary"
                     name="habitSummary"
                     type="text"
@@ -68,11 +58,11 @@ function ReusableForm(props){
                 {/* placeholder for tracker visual representation... */}
                 {/* <div class="mb-4 md:flex md:justify-between">
                   <div class="mb-4 md:mr-2 md:mb-0">
-                    <label class="block mb-2 text-sm font-bold text-gray-700" for="password">
+                    <label class="block mb-2 text-sm font-bold text-gray-400" for="password">
                       Password
                     </label>
                     <input
-                      class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                      class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-400 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                       id="password"
                       type="password"
                       placeholder="******************"
@@ -80,11 +70,11 @@ function ReusableForm(props){
                     <p class="text-xs italic text-red-500">Please choose a password.</p>
                   </div>
                   <div class="md:ml-2">
-                    <label class="block mb-2 text-sm font-bold text-gray-700" for="c_password">
+                    <label class="block mb-2 text-sm font-bold text-gray-400" for="c_password">
                       Confirm Password
                     </label>
                     <input
-                      class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                      class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-400 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                       id="c_password"
                       type="password"
                       placeholder="******************"
@@ -93,23 +83,23 @@ function ReusableForm(props){
                 </div> */}
                 <div class="mb-6 text-center">
                   <button
-                    class="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+                    class="w-full px-4 py-2 font-bold text-white btn glass hover:text-black rounded-full hover:bg-blue-800 focus:outline-none focus:shadow-outline"
                     type="submit">{props.buttonText}</button>
                 </div>
                 <hr class="mb-6 border-t" />
-                <div className="  rounded-b-box rounded-tr-box flex min-h-[6rem] min-w-[18rem] max-w-4xl flex-wrap items-center justify-center gap-2 overflow-x-hidden border bg-cover bg-top p-4 undefined">
-                  <button onClick={()=>props.onDelete()} className="btn btn-wide glass hover:bg-red-600 focus:ring-4 focus:ring-red-300">
+                <div className="   flex items-center justify-evenly gap-2     pb-4 ">
+                  <button onClick={()=>props.onDelete()} className="btn btn-wide w-96 hover:text-black glass hover:bg-red-600 focus:ring-4 focus:ring-red-300 -ml-10">
                     Delete
                   </button>
-                  <button onClick={()=>props.onBack()} className="btn btn-wide glass hover:bg-purple-700 border border-red-800 hover:text-black hover:shadow-lg hover:shadow-purple-500/50">
+                  <button onClick={()=>props.onBack()} className="btn btn-wide glass w-96 hover:bg-purple-700 border border-red-800 hover:text-black hover:shadow-lg hover:shadow-purple-500/50 -mr-10">
                     Back
                   </button>
                   {/* add back to list/home btn */}
                 </div>
               </form>
-            </div>
-          </div>
-        </div>
+
+
+
       </div>
     </div>
   </div>
