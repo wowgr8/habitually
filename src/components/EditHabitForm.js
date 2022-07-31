a9d2e28b83542296fc5abefc0c35038b1cfe43e3
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-30 17:33:22 -0700

    removed unused note and useState hooks

--- src/components/EditHabitForm.js
+++ src/components/EditHabitForm.js
@@ -6,9 +6,7 @@ import { Context } from '../utils/Context';
 import { useNavigate } from "react-router-dom";
 
 function EditHabitForm (){
-  // const [newHabit, setNewHabit] = useState(() => "")
-  // const [newSummary, setNewSummary] = useState(() =>"")
-  // const [newTimeFrame, setNewTimeFrame] = useState(() =>"")
+
   const { newHabit, setNewHabit,
           newSummary, setNewSummary,
           newTimeFrame, setNewTimeFrame,
@@ -16,7 +14,6 @@ function EditHabitForm (){
         } = useContext(Context);
 
   let navigate = useNavigate();
-
 
   const updateHabit = async(e) => {
     console.log(selectedHabit, "IN EDIT.js");
@@ -27,10 +24,6 @@ function EditHabitForm (){
                       }; 
     await updateDoc(doc(db, "user", selectedHabit), {...newFields}) 
     setSelectedHabit(selectedHabit); // updates placeholders in reusable form.
-
-    // call function which renders button to go back home(HabitLIst), giving the user the option to stay and edit the same habit once again.
-    // setSelectedHabit(); Only needed if navigating back to HabitLIst.
-    // navigate("/HabitList");
   }
 
   const deleteHabit = async() =>{
@@ -48,10 +41,8 @@ function EditHabitForm (){
   return(
     <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]" >
       <div className="px-6 pt-6 2xl:container">
-        {/* could this use a div with line 30 styling? WIP */}
         <div className="h-full  bg-white shadow-md rounded px-8 pt-16 pb-18 mb-4  flex-col ">
           <div class="overflow-x-auto relative">
-
             <ReusableForm 
               onSubmissionHandler = {updateHabit}
               onDelete= {deleteHabit}
